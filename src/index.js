e2eb1e18f1f6cc1548f226e71c8302d438e841da
VolZa <vvolza@gmail.com>
2023-05-05 20:59:07 +0300

    Add Librery

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/app/App';
+import { App } from 'components/App/App';
 // import './index.css';rt
 import { ThemeProvider } from 'styled-components';
 
