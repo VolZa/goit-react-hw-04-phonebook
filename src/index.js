6930ace1797a169e7ae79aa31afead15e1da0beb
VolZa <vvolza@gmail.com>
2023-05-05 20:44:02 +0300

    Переніс з goit-react-hw-04-phonebook-

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App/App';
-import './index.css';
+import { App } from 'components/app/App';
+// import './index.css';rt
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    primary: '#232323',
+    accent: '#6085d3',
+    white: '#fdfdfd',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
